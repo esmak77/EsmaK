f06010794eed409bde0d09d15f6d9b3fdb07c67f
esmak77 <ekajoshaj5@gmail.com>
2025-02-11 18:23:52 +0100

    Add files via upload

---
+++ 5thcode(name response).py
@@ -0,0 +1,13 @@
+# Write your code below this line 👇
+from operator import length_hint
+
+name = "Angela"
+print(name)
+
+name = "Jack"
+print(name)
+
+#print(len(input("What is your name?")))
+username = input("What is your name?")
+length = len(username)
+print(length)
