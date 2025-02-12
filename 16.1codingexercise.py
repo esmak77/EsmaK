a259cdd2b055630ad953bb6009852fa99599d735
esmak77 <ekajoshaj5@gmail.com>
2025-02-12 21:51:34 +0100

    Add files via upload

---
+++ 16.1codingexercise.py
@@ -0,0 +1,6 @@
+print("BMI Calculator")
+weight = float(input("What is your weight in kg?\n"))
+height = float(input("How tall are you in meters?\n"))
+bmi = (weight) / (height ** 2)
+print("Your BMI is: " + str(bmi))
+
