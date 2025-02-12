bd05d15a41f5f4932ed10ead988ea9562e1225e1
esmak77 <ekajoshaj5@gmail.com>
2025-02-12 10:00:18 +0100

    Add files via upload

---
+++ 15typeerror.py
@@ -0,0 +1,23 @@
+len("Hello")
+
+print(type("Hello"))
+print(type(123))
+print(type(123.456))
+print(type(True))
+
+print(int("123") + int("456"))
+
+int()
+float()
+str()
+bool()
+
+name_of_user = input("Enter your name")
+length_of_name = len(name_of_user)
+
+print(type("Number of letters in your name: ")) #str
+print(type(length_of_name)) #int
+str(length_of_name)
+
+print("Number of letters in your name: " + str(length_of_name))
+
