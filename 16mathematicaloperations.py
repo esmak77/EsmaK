c90816f5c6e868392ee467b9a5a2c28f03ca192c
esmak77 <ekajoshaj5@gmail.com>
2025-02-12 21:23:14 +0100

    Add files via upload

---
+++ 16mathematicaloperations.py
@@ -0,0 +1,16 @@
+print("My name is: " + str(12))
+print(123 + 456)
+print (7 - 3)
+print(9 * 7)
+print(5 / 3)
+print(5 // 3)
+print(6 ** 2)
+
+#PEMDASLR
+# ()
+# **
+# * OR /
+# + OR -
+
+print(3 * (3 + 3) / 3 - 3)
+
